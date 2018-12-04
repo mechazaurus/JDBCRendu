4135c65912b3e70a19aa42464877c323d0af0e3c
SwiiZe <irteza.sheikh24@gmail.com>
2018-12-04 15:26:17 +0100

    okok

---
+++ JDBCRendu/src/main.java
@@ -0,0 +1,18 @@
+import java.sql.SQLException;
+
+public class main
+{
+    public static void main(String [] args)
+    {
+        try
+        {
+            BDD bdd = new BDD("");
+
+        }
+        catch (SQLException sql)
+        {
+            System.out.println(sql);
+        }
+
+    }
+}
