bd84e94135ba3cc04bd0c636762c21a5d3755c9d
mechazaurus <beetle.buzzy@gmail.com>
2018-12-08 16:28:46 +0100

    DAO

---
+++ JDBCRendu/src/com/polytech/bsm/controler/LocalDAO.java
@@ -0,0 +1,33 @@
+package com.polytech.bsm.controler;
+
+import com.polytech.bsm.model.Local;
+
+public class LocalDAO extends DAO<Local> {
+
+	@Override
+	public Local find(Integer id) {
+		
+		Local local = null;
+		
+		
+		return local;
+	}
+
+	@Override
+	public Local create(Local obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Local update(Local obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Local obj) {
+
+	}
+
+}
--- JDBCRendu/src/com/polytech/bsm/launcher/Main.java
+++ JDBCRendu/src/com/polytech/bsm/launcher/Main.java
@@ -1,35 +1,27 @@
 package com.polytech.bsm.launcher;
 
-import java.sql.SQLException;
-import com.polytech.bsm.model.BDD;
-import com.polytech.bsm.view.MainApp;
+import com.polytech.bsm.controler.FlatDAO;
+import com.polytech.bsm.model.Flat;
 
 public class Main {
 	
     public static void main(String [] args) {
     	
-        try {
-            @SuppressWarnings("unused")
-			BDD bdd = new BDD();
-            /*
-            String query = "SELECT * FROM Flat";
-            ResultSet rs = bdd.executeQuery(query);
-
-            JPanel panel = new PanelSpecifiedSearch();
-            panel.setOpaque(true);
-            
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            frame.setTitle("Scrollable JTable");
-            frame.setContentPane(panel);
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);*/
-            
-            @SuppressWarnings("unused")
-			MainApp main = new MainApp();
-        } catch (SQLException sql) {
-            System.out.println(sql);
-        }
+        FlatDAO flatDAO = new FlatDAO();
+		Flat flat = new Flat();
+		for (int i = 1 ; i <= 5 ; i++) {
+			flat = flatDAO.find(i);
+			if (flat != null) {
+				System.out.println("===================");
+				System.out.println("Flat ID : " + flat.getFlatID());
+				System.out.println("Address : " + flat.getFlatAddress());
+				System.out.println("Description : " + flat.getFlatDescription());
+				System.out.println("State : " + flat.getFlatState().toString());
+				System.out.println("===================\n");
+			} else {
+				System.out.println("OOPSIES");
+			}
+		}
+		
     }
 }
