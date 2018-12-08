538460de53c3818e167b89de04bdbea017e50810
mechazaurus <beetle.buzzy@gmail.com>
2018-12-08 20:05:41 +0100

    Things

---
+++ JDBCRendu/src/com/polytech/bsm/controler/dao/LinksDAO.java
@@ -0,0 +1,12 @@
+package com.polytech.bsm.controler.dao;
+
+import java.sql.Connection;
+
+import com.polytech.bsm.model.ConnectionSQL;
+
+public class LinksDAO {
+	
+	private Connection connection = ConnectionSQL.getInstance();
+	
+	
+}
