9fa432f88098a4b4d09696d9ad6b07649faa92a1
SwiiZe <irteza.sheikh24@gmail.com>
2018-12-08 15:17:34 +0100

    s

--- JDBCRendu/src/com/polytech/bsm/view/MainApp.java
+++ JDBCRendu/src/com/polytech/bsm/view/MainApp.java
@@ -12,33 +12,18 @@ public class MainApp extends JFrame {
 
 	// Attributes
 	private static final long serialVersionUID = 1L;
-	private PanelFlatCreation panelFlatCreation;
-	private PanelSpecifiedSearch panelSpecifiedSearch;
 	private JButton addAppartmentButton;
 	private JButton searchAppartmentButton;
-	private JPanel mainAppPanel;
 	
 
 	public MainApp() {
-		
-		// Initialization
-		panelFlatCreation = new PanelFlatCreation();
-		panelSpecifiedSearch = new PanelSpecifiedSearch();
 		
 		// Main window setup
 		setResizable(false);
 		setSize(600, 500);
 		setLocationRelativeTo(null);
-		setVisible(true);
-		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		getContentPane().setLayout(null);
 		
 		addAppartmentButton = new JButton("Add Appartement");
-		addAppartmentButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		addAppartmentButton.setBounds(28, 33, 203, 65);
 		getContentPane().add(addAppartmentButton);
 
@@ -46,32 +31,13 @@ public class MainApp extends JFrame {
 		searchAppartmentButton.setBounds(373, 33, 203, 65);
 		getContentPane().add(searchAppartmentButton);
 		
-		mainAppPanel = panelSpecifiedSearch;
-		mainAppPanel.setBounds(28, 117, 541, 351);
-		this.getContentPane().add(mainAppPanel);
-		mainAppPanel = null;
-		repaint();
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+
+
+		setVisible(true);
 	}
-	
-	public void setInvisibleAllPanels()
-	{
-		panelFlatCreation.setVisible(false);
-		panelSpecifiedSearch.setVisible(false);
-	}
-	
-	public void setPanelFlatCreationON()
-	{
-		setInvisibleAllPanels();
-		mainAppPanel = panelFlatCreation;
-		panelFlatCreation.setVisible(true);
-	}
-	public void setSpecifiedSearchPanelON()
-	{
-		setInvisibleAllPanels();
-		mainAppPanel = panelSpecifiedSearch;
-		panelSpecifiedSearch.setVisible(true);
-	}
-	
-	
+
 	
 }
