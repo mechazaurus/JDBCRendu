10150f43645aa4ff5763d302969771f721ddb348
mechazaurus <beetle.buzzy@gmail.com>
2018-12-10 14:19:31 +0100

    Ok

--- JDBCRendu/src/com/polytech/bsm/launcher/Main.java
+++ JDBCRendu/src/com/polytech/bsm/launcher/Main.java
@@ -3,12 +3,14 @@ package com.polytech.bsm.launcher;
 import com.polytech.bsm.controler.MainAppController;
 import com.polytech.bsm.controler.dao.FlatDAO;
 
-import java.sql.SQLException;
-
 import com.polytech.bsm.controler.dao.LinksDAO;
 import com.polytech.bsm.controler.dao.LocalDAO;
+import com.polytech.bsm.controler.dao.SearchDAO;
 import com.polytech.bsm.model.MainAppModel;
 import com.polytech.bsm.view.MainAppView;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Main {
 	
@@ -17,6 +19,15 @@ public class Main {
         FlatDAO flatDAO = new FlatDAO();
         LocalDAO localDAO = new LocalDAO();
         LinksDAO linksDAO = new LinksDAO();
+        
+        SearchDAO searchDAO = new SearchDAO();
+        
+        ArrayList<Integer> ids = new ArrayList<>();
+        ids = searchDAO.searchFlat("bedroom", 2);
+        
+        for(Integer id: ids) {
+        	System.out.println(id);
+        }
         
         try
         {
